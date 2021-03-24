a000e4c9f23ac9a191d8e7255e15ac22d9cbcde7
Nicolas Demonte <nicolas@affinitic.be>
2021-03-24 14:46:52 +0100

    Add classification folder behavior DMS-402

---
+++ src/collective/classification/folder/behaviors/classification_folder.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+from collective.classification.folder import _
+from collective.classification.folder.content.vocabularies import ClassificationFolderSourceBinder
+from plone import schema
+from plone.autoform import directives as form
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.formwidget.autocomplete import AutocompleteMultiFieldWidget
+from plone.supermodel import model
+from zope.component import adapter
+from zope.interface import Interface
+from zope.interface import implementer
+from zope.interface import provider
+
+
+class IClassificationFolderMarker(Interface):
+    pass
+
+
+@provider(IFormFieldProvider)
+class IClassificationFolder(model.Schema):
+    """
+    """
+
+    form.widget(classification_folders=AutocompleteMultiFieldWidget)
+    classification_folders = schema.List(
+        title=_(u"Classification folders"),
+        description=_(u"List of folders / subfolders in which this content is filed"),
+        value_type=schema.Choice(
+            source=ClassificationFolderSourceBinder(),
+        ),
+        required=False,
+    )
+
+
+@implementer(IClassificationFolder)
+@adapter(IClassificationFolderMarker)
+class ClassificationFolder(object):
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def classification_folders(self):
+        if hasattr(self.context, 'classification_folders'):
+            return self.context.classification_folders
+        return None
+
+    @classification_folders.setter
+    def classification_folders(self, value):
+        self.context.classification_folders = value
