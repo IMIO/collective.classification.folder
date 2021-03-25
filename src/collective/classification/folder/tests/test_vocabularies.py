6cff2de45e4faf505fd27d0fe9562d5cfbfe1831
Nicolas Demonte <nicolas@affinitic.be>
2021-03-25 17:49:49 +0100

    Test classification folder source DMS-402

---
+++ src/collective/classification/folder/tests/test_vocabularies.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from collective.classification.folder.content.vocabularies import ClassificationFolderSource
+from collective.classification.folder.testing import COLLECTIVE_CLASSIFICATION_FOLDER_INTEGRATION_TESTING  # noqa
+from plone import api
+from plone.app.testing import login
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+
+import unittest
+
+
+class ClassificationFolderSourceTest(unittest.TestCase):
+
+    layer = COLLECTIVE_CLASSIFICATION_FOLDER_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer["portal"]
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+        user1 = api.user.create(email="user1@test.com", username="user1")
+        user2 = api.user.create(email="user2@test.com", username="user2")
+        group1 = api.group.create(
+            groupname="group1",
+            title=u"Group 1",
+        )
+        api.group.add_user(groupname="group1", user=user1)
+
+        self.folders = api.content.create(
+            container=self.portal,
+            type="Classification Folders",
+            id="folders",
+            title=u"Folders",
+        )
+        self.folder1 = api.content.create(
+            container=self.folders,
+            type="Classification Folder",
+            id="folder1",
+            title=u"Folder 1",
+            services_in_copy=["group1"],
+        )
+        self.folder2 = api.content.create(
+            container=self.folders,
+            type="Classification Folder",
+            id="folder2",
+            title=u"Folder 2",
+            services_in_copy=[],
+        )
+
+    def test_available_folders_as_manager(self):
+        source = ClassificationFolderSource(self.portal)
+        terms = [(term.value, term.title) for term in source]
+        self.assertEqual([("folder1", u"Folder 1"), ("folder2", u"Folder 2")], terms)
+
+    def test_available_folders_as_groupmember(self):
+        login(self.portal, "user1")
+        source = ClassificationFolderSource(self.portal)
+        terms = [(term.value, term.title) for term in source]
+        self.assertEqual([("folder1", u"Folder 1")], terms)
+
+    def test_available_folders_as_lambda(self):
+        login(self.portal, "user2")
+        source = ClassificationFolderSource(self.portal)
+        terms = [(term.value, term.title) for term in source]
+        self.assertEqual([], terms)
+
