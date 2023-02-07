58672225b8d7530a0baae45affef5fd15879851f
Stephan Geulette <s.geulette@imio.be>
2023-02-07 12:22:04 +0100

    Removed temporary chosen from upĝrades registry

--- src/collective/classification/folder/upgrades/upgrades.py
+++ src/collective/classification/folder/upgrades/upgrades.py
@@ -1,4 +1,6 @@
 from plone import api
+from Products.GenericSetup.interfaces import IUpgradeSteps
+from Products.GenericSetup.registry import GlobalRegistryStorage
 
 
 def to1001(context):
@@ -7,3 +9,7 @@ def to1001(context):
                                     dependency_strategy='new')
     gs.runAllImportStepsFromProfile('profile-collective.z3cform.select2:default',
                                     dependency_strategy='new')
+    upgrade_registry = GlobalRegistryStorage(IUpgradeSteps)
+    for profile in (u'collective.js.chosen:default', u'collective.z3cform.chosen:default'):
+        if profile in upgrade_registry.keys():
+            del upgrade_registry[profile]
